9c7419e8cc15c7a32bcf626c3aa552e7e8e57c63
Zayne Liu <llz981011@hotmail.com>
2021-04-29 01:44:13 +0100

    Update Room.java

--- src/main/java/zayne/psic_booking_system/models/Room.java
+++ src/main/java/zayne/psic_booking_system/models/Room.java
@@ -1,19 +1,30 @@
 package zayne.psic_booking_system.models;
 
+import zayne.psic_booking_system.models.Physician.Treatment;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
-
-import static zayne.psic_booking_system.models.Physician.WORKING_HRS_START;
+import java.util.HashSet;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Room {
     public static final ArrayList<Room> rooms = new ArrayList<>();
-    public static HashMap<String, Boolean> allSlots = new HashMap<>();
-    public static ArrayList<String> bookedSlots = new ArrayList<>();
+    public static HashMap<String, Boolean> allSlots;
+    public static int totalDays = 0;
+    // public static HashMap<String, Boolean> consultationSlots = new HashMap<>();
+    public static HashSet<String> allRooms = new HashSet<>();
+
     public String roomName = "";
     public RoomType roomType = RoomType.GENERAL;
 
     static {
+        allSlots = new HashMap<>();
+        var start = Calendar.getInstance();
+        start.set(2021, Calendar.MAY, 3);
+        var end = Calendar.getInstance();
+        end.set(2021, Calendar.MAY, 30);
+        totalDays = end.get(Calendar.DAY_OF_MONTH) - start.get(Calendar.DAY_OF_MONTH) + 1;
 
         // Room Data
         rooms.add(new Room("A", RoomType.GENERAL));
@@ -28,55 +39,78 @@ public class Room {
         this.roomName = roomName;
         this.roomType = roomType;
 
-        var start = Calendar.getInstance();
-        start.set(2021, Calendar.MAY, 3);
-        var end = Calendar.getInstance();
-        end.set(2021, Calendar.MAY, 30);
-
-        var days = end.get(Calendar.DAY_OF_MONTH) - start.get(Calendar.DAY_OF_MONTH) + 1;
-
-        for (int d = 1; d <= days; d++) {
+        for (int d = 1; d <= totalDays; d++) {
             // Slots init data.
             // key: Room on specific date and time.
-            // value: isBooked
-            allSlots.put("%s-%d-%s:1".formatted("May", d + 2, roomName), false);
-            allSlots.put("%s-%d-%s:2".formatted("May", d + 2, roomName), false);
-            allSlots.put("%s-%d-%s:3".formatted("May", d + 2, roomName), false);
-            allSlots.put("%s-%d-%s:4".formatted("May", d + 2, roomName), false);
-        }
-    }
+            // value: isAvailable
+            // "May-12_A_10-12" : true,
+            // "May-12_A_12-14" : true,
+            // "May-12_A_14-16" : false,
+            // "May-12_A_16-18" : true,
+            if (!allRooms.contains(roomName)) {
+                // "May-12_A_16-18" : true,
+                var date = "%s-%d".formatted("May", d + 2);
 
-    public static void book(String roomName, int date, int startTime) {
-        var slotNo = (startTime - WORKING_HRS_START) / 2 + 1;
-        var month = "May";
-        // Example key in hashmap `allSlots`
-        /* `May-10-A:1` */
-        var key = "%s-%d-%s:%d".formatted(month, date, roomName, slotNo);
-        var isBooked = allSlots.get(key);
-        if (!isBooked) {
-            allSlots.put(key, true);
-            bookedSlots.add(key);
-        }
-    }
-
-    public static ArrayList<Room> getAvailableRooms(Appointment appointment) {
-        var result = new ArrayList<Room>();
-
-        var roomType =
-                appointment.treatment.toString().split("_").length == 1
-                        ? RoomType.GENERAL
-                        : RoomType.REHABILITATION;
-        // TODO: return available rooms (treatment, date, time, not booked)
-
-        /* `May-10-A:1` */
-        for (var slot : allSlots.entrySet()) {
-
-            if (slot.getValue() == false) {
-                // if ()
+                allSlots.put("%s_%s_10-12".formatted(date, roomName), true);
+                allSlots.put("%s_%s_12-14".formatted(date, roomName), true);
+                allSlots.put("%s_%s_14-16".formatted(date, roomName), true);
+                allSlots.put("%s_%s_16-18".formatted(date, roomName), true);
             }
         }
+    }
 
-        // if () for (var room : rooms) {}
+    public Room() {}
+
+    public static Room getRoom(String roomName) {
+        var room = roomName.split("_")[0];
+        AtomicReference<Room> result = new AtomicReference<>(null);
+        rooms.forEach(
+                item -> {
+                    if (item.roomName.equals(room)) result.set(item);
+                });
+        return result.get();
+    }
+
+    public static ArrayList<String> getAvailableRooms(Calendar date, Treatment treatment) {
+        var result = new ArrayList<String>();
+
+        // May-12
+        var targetDate = "%s-%s".formatted("May", date.get(Calendar.DATE));
+        // 10-12
+        var targetTime =
+                "%s-%s"
+                        .formatted(
+                                date.get(Calendar.HOUR_OF_DAY), date.get(Calendar.HOUR_OF_DAY) + 2);
+        var targetRoomType =
+                treatment.name().split("_").length == 1
+                        ? RoomType.GENERAL
+                        : RoomType.REHABILITATION;
+
+        var generalList = new ArrayList<String>() {}; // A,B,C,D
+        var rehabList = new ArrayList<String>() {}; // Gym,Pool
+        for (var room : rooms) {
+            if (room.roomType == RoomType.GENERAL) generalList.add(room.roomName);
+            else rehabList.add(room.roomName);
+        }
+
+        allSlots.forEach(
+                (k, v) -> {
+                    var temp = k.split("_");
+                    var tDate = temp[0];
+                    var tRoom = temp[1];
+                    var tTime = temp[2];
+                    if (targetRoomType == RoomType.GENERAL) {
+                        if (tDate.equals(targetDate)
+                                && targetTime.equals(tTime)
+                                && generalList.contains(tRoom)
+                                && v) result.add("%s_%s".formatted(tRoom, tTime));
+                    } else {
+                        if (tDate.equals(targetDate)
+                                && targetTime.equals(tTime)
+                                && rehabList.contains(tRoom)
+                                && v) result.add("%s_%s".formatted(tRoom, tTime));
+                    }
+                });
 
         return result;
     }
